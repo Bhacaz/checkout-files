2f0c485d7e822e00aaa4a9d0c487be8a9f17bc36
Jean-Francis Bastien <bhacaz@gmail.com>
2020-08-18 21:08:52 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#FILES=${{ inputs.who-to-greet }}
+FILES=${{ inputs.who-to-greet }}
 
 IFS=' ' read -ra array <<< "$FILES"
 for i in "${array[@]}"
 do
   echo 'Downloading' $i'...'
-  curl https://raw.githubusercontent.com/Bhacaz/democraylist-be/master/"$i" -s -o "$i"
+  curl https://raw.githubusercontent.com/"$GITHUB_REPOSITORY"/master/"$i" -s -o "$i"
 done
