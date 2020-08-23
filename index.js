23f998552daaf480c39a9cdeac24d65ace0b4db7
Jean-Francis Bastien <bhacaz@gmail.com>
2020-08-22 21:14:49 -0400

    use https

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core');
-const http = require('http');
+const https = require('https');
 const fs = require('fs');
 
 const files = core.getInput('files');
@@ -7,7 +7,7 @@ const repository = process.env['GITHUB_REPOSITORY']
 
 function dowloadFile(fileName) {
     const file = fs.createWriteStream(fileName);
-    http.get("https://raw.githubusercontent.com/" + repository + '/master/' + fileName, function(response) {
+    https.get("https://raw.githubusercontent.com/" + repository + '/master/' + fileName, function(response) {
         response.pipe(file);
     });
 }
