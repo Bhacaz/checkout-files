1427b93996d05b974a6cf3d710070e54db02dd41
Jean-Francis Bastien <bhacaz@gmail.com>
2020-08-22 21:36:00 -0400

    Clean up

--- script.sh
+++
@@ -1,8 +0,0 @@
-FILES=${{ inputs.files }}
-
-IFS=' ' read -ra array <<< "$FILES"
-for i in "${array[@]}"
-do
-  echo 'Downloading' $i'...'
-  curl https://raw.githubusercontent.com/"$GITHUB_REPOSITORY"/master/"$i" -s -o "$i"
-done
